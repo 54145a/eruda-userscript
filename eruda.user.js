24048c988be55b98804269b31ef530292e9b20ab
145a <133004095+54145a@users.noreply.github.com>
2025-02-23 11:07:53 +0800

    update eruda.user.js

--- eruda.user.js
+++ eruda.user.js
@@ -2,11 +2,13 @@
 // @name         Eruda
 // @namespace    https://54145a.github.io/
 // @version      2025-02-22
+// @downloadURL  https://github.com/54145a/eruda-userscript/raw/refs/heads/main/eruda.user.js
+// @updateURL    https://github.com/54145a/eruda-userscript/raw/refs/heads/main/eruda.user.js
 // @description  Simple userscript to load eruda.
 // @author       145a
 // @match        *://*/*eruda=1*
 // @run-at       document-body
-// @icon         https://www.tampermonkey.net/favicon.ico
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://cdn.jsdelivr.net/npm/eruda
 // @grant        none
 // @connect      *
