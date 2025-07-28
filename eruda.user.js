143f4ff7f8dedd1ce5f9fbda353f0812c0a2d378
145a <133004095+54145a@users.noreply.github.com>
2025-07-28 16:08:08 +0800

    Update eruda.user.js

--- eruda.user.js
+++ eruda.user.js
@@ -1,21 +1,30 @@
 // ==UserScript==
 // @name         Eruda
-// @namespace    https://54145a.github.io/
-// @version      2025-02-22
+// @description  Simple userscript that loads eruda console.
+// @match        *://*/*?*loaderuda=1*
+// @version      2025-06-20
+// @namespace    https://github.com/54145a
 // @downloadURL  https://github.com/54145a/eruda-userscript/raw/refs/heads/main/eruda.user.js
 // @updateURL    https://github.com/54145a/eruda-userscript/raw/refs/heads/main/eruda.user.js
-// @description  Simple userscript to load eruda.
-// @author       145a
-// @match        *://*/*eruda=1*
-// @run-at       document-body
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://cdn.jsdelivr.net/npm/eruda
 // @grant        none
-// @connect      *
-
 // ==/UserScript==
-
-(function() {
-    window.eruda.init();
-    window.eruda.show();
-})();
+(function () {
+    //GM_registerMenuCommand
+    //GM_unregisterMenuCommand
+    "use strict";
+    if(!window.eruda) return;
+    try {
+        let initCmdId;
+        function initEruda() {
+            //GM_unregisterMenuCommand(initCmdId);
+            window.eruda.init();
+            window.eruda.show();
+            //window.eruda.get("console").config.set("displayIfErr", true);
+        }
+        //initCmdId = GM_registerMenuCommand("init()", initEruda)
+        initEruda();
+    } catch (e) {
+        prompt("", e);
+    }
+}());
